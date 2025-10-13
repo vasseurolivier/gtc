eed47b44d773b65844c663485aeeb5e0f648385d
olivier vasseur <vasseurolivier86@gmail.com>
2025-10-13 08:07:18 +0000

    dans l onglet internet pourrait t on appliquer mon logo commercial ?

--- src/components/layout/favicon.tsx
+++ src/components/layout/favicon.tsx
@@ -1,12 +1,23 @@
 
 'use client';
 
+import React, { useContext, useEffect, useState } from 'react';
 import Head from 'next/head';
+import { CompanyInfoContext } from '@/context/company-info-context';
 
 export function Favicon() {
+  const [faviconUrl, setFaviconUrl] = useState('/favicon.ico'); // Fallback
+  const companyInfoContext = useContext(CompanyInfoContext);
+
+  useEffect(() => {
+    if (companyInfoContext?.companyInfo.publicLogo) {
+      setFaviconUrl(companyInfoContext.companyInfo.publicLogo);
+    }
+  }, [companyInfoContext]);
+
   return (
     <Head>
-      <link rel="icon" href="/logo.png" type="image/png" sizes="any" />
+      <link rel="icon" href={faviconUrl} type="image/png" sizes="any" />
     </Head>
   );
 }
