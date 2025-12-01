ff6bdc72699fd4e2a858a128c44a22c0bccae1ae
olivier vasseur <vasseurolivier86@gmail.com>
2025-12-01 17:43:19 +0000

    le logo n est toutjours pas visible sur les pro forma, reduit la taille

--- src/app/admin/quotes/[id]/quote-preview.tsx
+++ src/app/admin/quotes/[id]/quote-preview.tsx
@@ -104,9 +104,9 @@ export function QuotePreview({ quote, customer, products }: { quote: Quote, cust
             <div className="bg-white rounded-lg shadow-lg border">
                 <div ref={printRef} className="px-8 py-10 pb-32">
                     <header className="flex justify-between items-start pb-8 mb-8 border-b">
-                        <div className="w-1/3 relative h-16">
+                        <div className="w-1/3">
                            {companyInfo.logo && 
-                                <Image src={companyInfo.logo} alt="Company Logo" fill style={{objectFit: "contain", objectPosition: "left"}}/>
+                                <Image src={companyInfo.logo} alt="Company Logo" width={120} height={60} style={{objectFit: "contain", objectPosition: "left"}}/>
                             }
                         </div>
                         <div className="text-right w-2/3">
