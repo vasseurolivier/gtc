e14e55f56b9d1e0fd8e8695573810cb996b1cf5a
olivier vasseur <vasseurolivier86@gmail.com>
2025-12-08 05:42:04 +0000

    13:34:44.593 Running build in Washington, D.C., USA (East) – iad1 13:34:

--- src/app/[locale]/(public)/layout.tsx
+++ src/app/[locale]/(public)/layout.tsx
@@ -7,12 +7,12 @@ import { PublicProviders } from '@/components/layout/public-providers';
 
 export default async function PublicLayout({
   children,
-  params: { locale },
+  params,
 }: {
   children: React.ReactNode;
   params: { locale: Locale };
 }) {
-  const dictionary = await getDictionary(locale);
+  const dictionary = await getDictionary(params.locale);
 
   return (
     <PublicProviders>
