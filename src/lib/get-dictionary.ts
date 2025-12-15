688142caedcf10c90a7e8e3912479e06537e5cdd
olivier vasseur <vasseurolivier86@gmail.com>
2025-12-15 09:10:56 +0000

    17:02:31.901 Running build in Washington, D.C., USA (East) – iad1 17:02:

--- src/lib/get-dictionary.ts
+++ src/lib/get-dictionary.ts
@@ -1,6 +1,6 @@
+
 import 'server-only'
 import type { Locale } from '../i18n-config'
-import { i18n } from '../i18n-config'
 
 const dictionaries = {
   en: () => import('../dictionaries/en.json').then((module) => module.default),
@@ -8,5 +8,5 @@ const dictionaries = {
 }
 
 export const getDictionary = async (locale: Locale) => {
-    return i18n.locales.includes(locale) ? dictionaries[locale]() : dictionaries.fr();
+  return locale === 'en' ? dictionaries.en() : dictionaries.fr()
 }
