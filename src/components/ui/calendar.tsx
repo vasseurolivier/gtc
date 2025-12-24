d2bf26a6d150c88fe75013f6df1d1f62925acacc
olivier vasseur <vasseurolivier86@gmail.com>
2025-12-24 16:11:26 +0000

    text Je travaille sur une application Next.js avec TypeScript et j'utili

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -55,8 +55,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        iconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        iconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
