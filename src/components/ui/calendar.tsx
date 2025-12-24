a8505e4f48336b7e7c6a95f662ae21a423ce7b97
olivier vasseur <vasseurolivier86@gmail.com>
2025-12-24 15:44:04 +0000

    23:41:42.634 Running build in Washington, D.C., USA (East) – iad1 23:41:

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -53,8 +54,10 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      iconLeft={<ChevronLeft className="h-4 w-4" />}
-      iconRight={<ChevronRight className="h-4 w-4" />}
+      components={{
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+      }}
       {...props}
     />
   )
