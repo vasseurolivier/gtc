6d98184d985c9e7abd602207bdc2549a5e795249
olivier vasseur <vasseurolivier86@gmail.com>
2025-12-24 17:32:11 +0000

    A nouveuau sauf sur la page d acceuil ou c est deja le cas, sur les autr

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -77,7 +77,7 @@ export function Header() {
 
   const headerClasses = cn(
     "fixed top-0 z-50 w-full transition-all duration-300",
-    isScrolled || pathname !== '/'
+    isScrolled
       ? "border-b bg-zinc-950/90 backdrop-blur supports-[backdrop-filter]:bg-zinc-950/80"
       : "bg-transparent border-transparent"
   );
@@ -93,14 +93,14 @@ export function Header() {
     }
 
     return cn(
-      "relative transition-colors font-semibold text-lg text-white",
+      "relative transition-colors font-semibold text-xl text-white",
       "after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:h-[2px] after:w-0 after:bg-primary after:transition-all after:duration-300 hover:after:w-full",
       isActive ? "text-white after:w-full" : "hover:text-white/90"
     );
   };
   
   const dropdownTriggerClasses = cn(
-    "relative flex items-center gap-1 transition-colors focus:outline-none font-semibold text-lg text-white",
+    "relative flex items-center gap-1 transition-colors focus:outline-none font-semibold text-xl text-white",
      "after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:h-[2px] after:w-0 after:bg-primary after:transition-all after:duration-300 hover:after:w-full",
     activePath.startsWith('/services')
       ? "text-white after:w-full"
@@ -110,8 +110,8 @@ export function Header() {
 
   return (
     <header className={headerClasses}>
-      <div className="container flex h-16 items-center">
-        <div className="flex items-center flex-1">
+      <div className="container flex h-16 items-center justify-between">
+        <div className="flex items-center">
             <Link href={'/'} className="flex items-center space-x-2">
                 {publicLogo ? (
                 <Image src={publicLogo} alt="Company Logo" width={50} height={50} className="object-contain invert brightness-0" />
