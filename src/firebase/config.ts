0c7bd61625c64e437e99b7d96796d39d7e074a01
olivier vasseur <vasseurolivier86@gmail.com>
2026-02-03 19:00:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4928604682-ea1ec",
+  "appId": "1:913801169761:web:0a43c3c13f0549b99c9320",
+  "apiKey": "AIzaSyDbIhhHeFmrFsHAL6U-ht1AgTQjvGG3otw",
+  "authDomain": "studio-4928604682-ea1ec.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "913801169761"
+};
