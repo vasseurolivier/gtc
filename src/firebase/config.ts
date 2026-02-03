036b94aec7cf9267c916b542afbf8ffdea008c08
olivier vasseur <vasseurolivier86@gmail.com>
2026-02-03 20:02:53 +0000

    dans les espace client on ne peu toujours pas uploader d image voila le

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:913801169761:web:0a43c3c13f0549b99c9320",
   "apiKey": "AIzaSyDbIhhHeFmrFsHAL6U-ht1AgTQjvGG3otw",
   "authDomain": "studio-4928604682-ea1ec.firebaseapp.com",
+  "storageBucket": "studio-4928604682-ea1ec.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "913801169761"
 };
