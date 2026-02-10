20232bd530a94341e7f30ef7e320da487f6bb500
olivier vasseur <vasseurolivier86@gmail.com>
2026-02-10 08:17:40 +0000

    16:13:36.525 Running build in Washington, D.C., USA (East) – iad1 16:13:

--- src/app/admin/registered-clients/page.tsx
+++ src/app/admin/registered-clients/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { useEffect, useState, useContext } from 'react';
@@ -235,7 +234,8 @@ export default function RegisteredClientsPage() {
               onChange={(e) => setSearch(e.target.value)}
             />
           </CardContent>
-        </div>
+        </Card>
+      </div>
 
       <Card className="border-none shadow-md overflow-hidden bg-white">
         <CardContent className="p-0">
